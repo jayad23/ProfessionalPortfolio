363824b2c9ca42dba8ffa6d4795c74fe1f669cd6
jayad23 <jaidervanegas@live.com>
2021-12-01 12:20:54 -0500

    First Commit

---
+++ src/Views/About.jsx
@@ -0,0 +1,30 @@
+import React, {useContext} from 'react'
+import { Link } from 'react-router-dom'
+import '../Styles/About.css'
+import ProfesionaInfoContext from '../Context/ProfesionalInfoContext'
+
+const About = () => {
+    const { Data } = useContext(ProfesionaInfoContext)
+    return (
+        <div className="container-about">
+            <div className="bg-about">
+                <img src={Data.images.BGAbout} alt="kike" />
+            </div>
+            <div className="info-subContainer">
+                <div className="text-about">
+                    <h2>A B O U T...</h2>
+                    <p>{Data.personal.BIO}</p>
+                    <p>{Data.personal.BIO2}</p>
+                </div>
+                <div className="history-container">
+                    <Link to="#">Academic Information</Link>
+                    <Link to="#">Work History</Link>
+                    <Link to="#">Skills</Link>
+                </div>
+                <Link to="/" className="back"><i class="fas fa-arrow-left"></i></Link>
+            </div>
+        </div>
+    )
+}
+
+export default About
