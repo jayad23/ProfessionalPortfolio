1117d52bedd3ef40a052b5727b07801df54354fe
jayad23 <jaidervanegas@live.com>
2021-12-10 13:03:08 -0500

    changing skills

--- src/components/AdditionalText.jsx
+++ src/components/AdditionalText.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Flip } from 'react-reveal'
 import LibraryReact from './LibraryReact'
 const AdditionalText = ({ react, js, css, html5, tailwind, sass, python, node, MySQL }) => {
-
-  console.log(react, js, css, html5, tailwind, sass, python, node, MySQL)
+  
   return (
     <div className="additionalText">
       {
